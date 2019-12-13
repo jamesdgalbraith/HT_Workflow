1b8879eba67eb12ffe39ee21775c2bf992e64241
jamesdgalbraith <galbraithenator@gmail.com>
2019-12-13 14:23:49 +1030

    Edited comments

--- scripts/LINE_finder.R
+++ scripts/LINE_finder.R
@@ -1,3 +1,5 @@
+# script to identify intact, poorly annotated LINEs in CARP output
+
 library(tidyverse)
 library(plyranges)
 library(BSgenome)
@@ -20,8 +22,6 @@ candidate_names <- consensus %>%
   dplyr::mutate(seqnames = gsub("#.*", "", seqnames)) # %>%
   # dplyr::filter(seqnames %in% c("family098280_consensus", "family137632_consensus", "family137633_consensus"))
 
-##### APPROACH 1 #####
-
 # create tibble of above granges object
 candidates <- candidate_names  %>%
   plyranges::as_granges()
@@ -42,17 +42,15 @@ names(candidate_seqs) <- candidate_names$seqnames
 Biostrings::writeXStringSet(x = candidate_seqs, filepath = "raw_data/candidate.fa")
 
 # run rpstblastn
-system("rpstblastn -db ~/Databases/localrpsb/db/Pfam -outfmt \"6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen\" -evalue 0.01 -query raw_data/candidate.fa -out raw_data/candidate_pfam.out")
-system("rpstblastn -db ~/Databases/localrpsb/db/Cdd_NCBI -outfmt \"6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen\" -evalue 0.01 -query raw_data/candidate.fa -out raw_data/candidate_cdd.out")
+system("rpstblastn -db ~/Databases/localrpsb/db/Cdd -outfmt \"6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen\" -evalue 0.01 -query raw_data/candidate.fa -out raw_data/candidate_cdd.out")
 
-# read in rpstblastn data
+# read in rpstblastn data, rename and filter out hits less that 80% of domain present
 rpstblastn_out <- read_tsv("raw_data/candidate_cdd.out", col_names = c("seqnames", "sseqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore", "qlen", "slen")) %>%
-  rbind(read_tsv("raw_data/candidate_pfam.out", col_names = c("seqnames", "sseqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore", "qlen", "slen"))) %>%
   dplyr::mutate(strand = case_when(qend > qstart ~ "+", qend < qstart ~ "-"), sseqid = gsub("gnl\\|CDD\\|", "", sseqid)) %>%
   arrange(seqnames, qstart) %>%
   dplyr::filter((send - sstart + 1) / slen >= 0.8)
 
-# determine sequences containing both RT and EN
+# determine intact LINEs (sequences containing both RT and EN) and convert to Granges
 rpstblastn_en <- rpstblastn_out %>%
   filter(sseqid %in% c(308788, 316995, 197310, 197311)) # endonucleases
 rpstblastn_rt <- rpstblastn_out %>%
@@ -64,11 +62,12 @@ LINEs <- rpstblastn_out %>%
   inner_join(candidate_names) %>%
   plyranges::as_granges()
 
+# create names for intact LINEs
 LINE_names <- LINEs %>%
   as_tibble() %>%
   mutate(seqnames = as.character(seqnames))
 
+# extract and name intact LINEs seqs from all seq
 LINE_seqs <- Biostrings::getSeq(consensus_seq, LINEs)
-
-# name candidate sequences
 names(LINE_seqs) <- LINE_names$seqnames
+
