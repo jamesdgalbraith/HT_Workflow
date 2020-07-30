4af4f284aa60e957559bf15a3a69ef4dd1aa451e
jamesdgalbraith <galbraithenator@gmail.com>
2020-07-30 14:56:37 +0930

    updated search and ploting method

--- TreeBuilding/consensus_plotter.R
+++ TreeBuilding/consensus_plotter.R
@@ -1,119 +1,59 @@
 library(tidyverse)
-library(plyranges)
-library(BSgenome)
-library(gridExtra)
-library(grid)
-library(svglite)
-#perform the blast
-blast=read.table(text=system("blastn -query ~/Analysis/Snake/HT_Workflow/Proto2-Snek/Proto2-Snek.fasta -db ~/Analysis/Genomes/Aipysurus_laevis/assembly_20171114.fasta -evalue 1e-07 -outfmt 6", intern = TRUE)) %>%
+install.packages("svglite")
+
+# set query and genome
+genome_path <- "~/Genomes/Reptiles/Laticauda_colubrina/latCor_1.0.fasta"
+query_repeat <- "~/HT_Workflow/RTE-Kret/RTE-Kret.fasta"
+
+# perform blastn search
+blastn <- read_tsv(system(paste0("blastn -num_threads 4 -dust yes -query ", query_repeat, " -db ", genome_path, " -outfmt \"6 sseqid qseqid qstart qend sstart send pident qcovs bitscore length mismatch evalue qlen slen\" -task megablast"), intern = TRUE), col_names = c("seqnames", "qseqid",  "qstart", "qend", "sstart", "send", "pident", "qcovs", "bitscore", "length", "mismatch", "evalue", "qlen", "slen")) %>%
   as_tibble() %>%
-  filter(V3 >= 97) %>%
-  mutate(V3 = 100-V3)
+  dplyr::filter(length >= 50) %>%
+  mutate(div = 100 - pident, d = mismatch/length, jc_dist = (-3 / 4) * log(1 - (4 * d / 3)), jc_dist_round = as.integer(100 * round(jc_dist, digits = 2))) %>%
+  arrange(-bitscore)
 
-#TE consensus size
-cons_len=system("grep -v '>'  ~/Analysis/Snake/HT_Workflow/Proto2-Snek/Proto2-Snek.fasta  | wc | awk '{print $3-$1}'", intern = TRUE)
-#list of almost full length fragments
-full=blast %>% filter(V4 >= as.double(cons_len) * 0.9)
+query_width <- blastn$qlen[1]
 
-# create coverage tibble
-coverage=matrix(rep(0, length(blast$V1)*as.numeric(cons_len)), byrow = T, ncol = as.numeric(cons_len))
-for(i in 1:length(blast$V1)){
-  coverage[i,]<-c(rep(0,blast$V7[i]-1),rep(1,abs(blast$V8[i]-blast$V7[i])+1), rep(0,as.numeric(cons_len)-blast$V8[i]))
+# select near full length repeats
+blastn_fl <- blastn %>%
+  filter(length >= qlen * 0.9)
+
+# calculate coverage of each bp
+bp_coverage <- matrix(rep(0, length(blastn$seqnames)*as.numeric(query_width)), byrow = T, ncol = as.numeric(query_width))
+
+for(k in 1:length(blastn$seqnames)){
+  bp_coverage[k,]<-c(rep(0,blastn$qstart[k]-1),rep(1,abs(blastn$qend[k]-blastn$qstart[k])+1), rep(0,as.numeric(query_width)-blastn$qend[k]))
 }
-coverage <- tibble(y = colSums(coverage))
-coverage$x <- 0:(nrow(coverage)-1)
 
-p1 <- ggplot() + geom_segment(aes(x=blast$V8, xend=blast$V7, y = blast$V3, yend = blast$V3), colour = "grey43", size = 0.5) +
-  geom_segment(aes(x=full$V8, xend=full$V7, y = full$V3, yend = full$V3), colour = "red", size = 1) + geom_line(mapping = aes(x = coverage$x, y = coverage$y/max(coverage$y) * 3), colour = "blue") +
-  scale_y_continuous(expand = c(0,0), name = "Divergence from consensus", lim = c(0,3.1), sec.axis = sec_axis(~(./max(blast$V3)*max(coverage$y)), name = "Consensus coverage (bp)")) +
-  scale_x_continuous(name = "Consensus sequence", expand = c(0,0)) + theme_bw() +
-  labs(title = "Proto2-Snek") + theme(plot.title = element_text(hjust = 0.5))
-p1
-ggsave(filename = "coverage_plots/Proto2-Snek_coverage.svg", plot = p1, width = 100, height = 100, units = "mm")
+# convert matrix to tibble
+bp_coverage_tbl <- tibble(bp_x = 1:query_width, bp_y = colSums(bp_coverage)) %>%
+  mutate(bp_y_scaled = max(blastn$div) * bp_y / max(bp_y))
 
+blastn %>%
+  filter(qend < 2080)
 
-##### RTE-Snek
-#perform the blast
-blast=read.table(text=system("blastn -query ~/Analysis/Snake/HT_Workflow/RTE-Snek/RTE-Snek.fasta -db ~/Analysis/Genomes/Aipysurus_laevis/assembly_20171114.fasta -evalue 1e-07 -outfmt 6", intern = TRUE)) %>%
-  as_tibble() %>%
-  filter(V3 >= 97) %>%
-  mutate(V3 = 100-V3)
+# plot coverage
+ggplot() + geom_segment(data = blastn, aes(x = qstart, xend = qend, y = 100 - pident, yend = 100 - pident), colour = "black") + # plot all repeats
+  
+  geom_segment(data = blastn_fl, aes(x = qstart, xend = qend, y = 100 - pident, yend = 100 - pident), colour = "#db7800", size = 1.5) + # plot full length repeats
+  geom_line(data = bp_coverage_tbl, aes(bp_x, bp_y_scaled), colour = "#006edb", size = 1.5) + # plot coverage data
+  scale_y_continuous(sec.axis = sec_axis(trans = ~ . * max(bp_coverage_tbl$bp_y)/3, name = "coverage"), # add second axis
+                     limits = c(-(max(blastn$div) *0.01),(max(blastn$div) *1.01)),  # scale for second axis
+                     expand = c(0,0)) + # don't expand axis
+  scale_x_continuous(expand = c(0,0)) + # don't expand axis
+  theme_bw() +
+  labs(x = "bp", y = "pairwise divergence from consensus", title = blastn$qseqid[1])
 
-#TE consensus size
-cons_len=system("grep -v '>'  ~/Analysis/Snake/HT_Workflow/RTE-Snek/RTE-Snek.fasta  | wc | awk '{print $3-$1}'", intern = TRUE)
-#list of almost full length fragments
-full=blast %>% filter(V4 >= as.double(cons_len) * 0.9)
+ggsave("TreeBuilding/RTE-Kret_coverage.svg", device = "svg", height = 10, width = 10, units = "cm")
 
-# create coverage tibble
-coverage=matrix(rep(0, length(blast$V1)*as.numeric(cons_len)), byrow = T, ncol = as.numeric(cons_len))
-for(i in 1:length(blast$V1)){
-  coverage[i,]<-c(rep(0,blast$V7[i]-1),rep(1,abs(blast$V8[i]-blast$V7[i])+1), rep(0,as.numeric(cons_len)-blast$V8[i]))
+# plot coverage
+jc_dist_mat <- matrix(nrow = length(blastn$seqnames), ncol = max(blastn$jc_dist_round) + 1)
+
+for(k in 1:length(blastn$seqnames)){
+  jc_dist_mat[k,] <-c(rep(0,blastn$jc_dist_round[k]),blastn$length[k], rep(0,max(blastn$jc_dist_round)-blastn$jc_dist_round[k]))
 }
-coverage <- tibble(y = colSums(coverage))
-coverage$x <- 0:(nrow(coverage)-1)
 
-p2 <- ggplot() + geom_segment(aes(x=blast$V8, xend=blast$V7, y = blast$V3, yend = blast$V3), colour = "grey43", size = 0.5) +
-  geom_segment(aes(x=full$V8, xend=full$V7, y = full$V3, yend = full$V3), colour = "red", size = 1) + geom_line(mapping = aes(x = coverage$x, y = coverage$y/max(coverage$y) * 3), colour = "blue") +
-  scale_y_continuous(expand = c(0,0), name = "Divergence from consensus", lim = c(0,3.1), sec.axis = sec_axis(~(./max(blast$V3)*max(coverage$y)), name = "Consensus coverage (bp)")) +
-  scale_x_continuous(name = "Consensus sequence", expand = c(0,0)) + theme_bw() +
-  labs(title = "RTE-Snek") + theme(plot.title = element_text(hjust = 0.5))
-p2
-ggsave(filename = "coverage_plots/RTE-Snek_coverage.svg", plot = p2, width = 100, height = 100, units = "mm")
+jc_dist_tibble <- tibble(jc_dist = 0:max(blastn$jc_dist_round), bp = colSums(jc_dist_mat)) %>%
+  mutate(jc_dist = jc_dist/100)
 
-##### Rex1-Snek_1
-#perform the blast
-blast=read.table(text=system("blastn -query ~/Analysis/Snake/HT_Workflow/Rex1-Snek_1/Rex1-Snek_1.fasta -db ~/Analysis/Genomes/Aipysurus_laevis/assembly_20171114.fasta -evalue 1e-07 -outfmt 6", intern = TRUE)) %>%
-  as_tibble() %>%
-  filter(V3 >= 97) %>%
-  mutate(V3 = 100-V3)
-
-#TE consensus size
-cons_len=system("grep -v '>'  ~/Analysis/Snake/HT_Workflow/Rex1-Snek_1/Rex1-Snek_1.fasta  | wc | awk '{print $3-$1}'", intern = TRUE)
-#list of almost full length fragments
-full=blast %>% filter(V4 >= as.double(cons_len) * 0.9)
-
-# create coverage tibble
-coverage=matrix(rep(0, length(blast$V1)*as.numeric(cons_len)), byrow = T, ncol = as.numeric(cons_len))
-for(i in 1:length(blast$V1)){
-  coverage[i,]<-c(rep(0,blast$V7[i]-1),rep(1,abs(blast$V8[i]-blast$V7[i])+1), rep(0,as.numeric(cons_len)-blast$V8[i]))
-}
-coverage <- tibble(y = colSums(coverage))
-coverage$x <- 0:(nrow(coverage)-1)
-
-p3 <- ggplot() + geom_segment(aes(x=blast$V8, xend=blast$V7, y = blast$V3, yend = blast$V3), colour = "grey43", size = 0.5) +
-  geom_segment(aes(x=full$V8, xend=full$V7, y = full$V3, yend = full$V3), colour = "red", size = 1) + geom_line(mapping = aes(x = coverage$x, y = coverage$y/max(coverage$y) * 3), colour = "blue") +
-  scale_y_continuous(expand = c(0,0), name = "Divergence from consensus", lim = c(0,3.1), sec.axis = sec_axis(~(./max(blast$V3)*max(coverage$y)), name = "Consensus coverage (bp)")) +
-  scale_x_continuous(name = "Consensus sequence", expand = c(0,0)) + theme_bw() +
-  labs(title = "Rex1-Snek_1") + theme(plot.title = element_text(hjust = 0.5))
-p3
-ggsave(filename = "coverage_plots/Rex1-Snek_1_coverage.svg", plot = p3, width = 100, height = 100, units = "mm")
-
-##### Rex1-Snek_2
-#perform the blast
-blast=read.table(text=system("blastn -query ~/Analysis/Snake/HT_Workflow/Rex1-Snek_2/Rex1-Snek_2.fasta -db ~/Analysis/Genomes/Aipysurus_laevis/assembly_20171114.fasta -evalue 1e-07 -outfmt 6", intern = TRUE)) %>%
-  as_tibble() %>%
-  filter(V3 >= 97) %>%
-  mutate(V3 = 100-V3)
-
-#TE consensus size
-cons_len=system("grep -v '>'  ~/Analysis/Snake/HT_Workflow/Rex1-Snek_2/Rex1-Snek_2.fasta  | wc | awk '{print $3-$1}'", intern = TRUE)
-#list of almost full length fragments
-full=blast %>% filter(V4 >= as.double(cons_len) * 0.9)
-
-# create coverage tibble
-coverage=matrix(rep(0, length(blast$V1)*as.numeric(cons_len)), byrow = T, ncol = as.numeric(cons_len))
-for(i in 1:length(blast$V1)){
-  coverage[i,]<-c(rep(0,blast$V7[i]-1),rep(1,abs(blast$V8[i]-blast$V7[i])+1), rep(0,as.numeric(cons_len)-blast$V8[i]))
-}
-coverage <- tibble(y = colSums(coverage))
-coverage$x <- 0:(nrow(coverage)-1)
-
-p4 <- ggplot() + geom_segment(aes(x=blast$V8, xend=blast$V7, y = blast$V3, yend = blast$V3), colour = "grey43", size = 0.5) +
-  geom_segment(aes(x=full$V8, xend=full$V7, y = full$V3, yend = full$V3), colour = "red", size = 1) + geom_line(mapping = aes(x = coverage$x, y = coverage$y/max(coverage$y) * 3), colour = "blue") +
-  scale_y_continuous(expand = c(0,0), name = "Divergence from consensus", lim = c(0,3.1), sec.axis = sec_axis(~(./max(blast$V3)*max(coverage$y)), name = "Consensus coverage (bp)")) +
-  scale_x_continuous(name = "Consensus sequence", expand = c(0,0)) + theme_bw() +
-  labs(title = "Rex1-Snek_2") + theme(plot.title = element_text(hjust = 0.5))
-
-p4
-ggsave(filename = "coverage_plots/Rex1-Snek_2_coverage.svg", plot = p4, width = 100, height = 100, units = "mm")
-
+ggplot(jc_dist_tibble, aes(jc_dist, bp)) + geom_bar(colour="black", fill="#DD8888", stat="identity")
